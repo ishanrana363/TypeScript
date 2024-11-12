51d6e316a251f70b6e2b5a71d23b3a0a2d3cf103
ishanrana363 <ishanrana363@gmail.com>
2024-11-12 08:09:39 +0600

    build in data type completed

---
+++ build-in-data-types/index.js
@@ -0,0 +1,18 @@
+// string number boolean void undefined null ;
+var firstName;
+var lastName;
+var isActive;
+var age;
+firstName = "John";
+lastName = " Doe";
+isActive = true;
+age = 30;
+var userName = firstName.concat(lastName).split("");
+console.log(userName);
+console.log("First Name: ".concat(firstName, " ").concat(lastName, " ").concat(isActive, " ").concat(age));
+// let id;
+function add() {
+    console.log("add");
+}
+;
+add();
