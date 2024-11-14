a7a58992cc71f4ddf359bb442d3ca6d8c3257811
ishanrana363 <ishanrana363@gmail.com>
2024-11-14 08:25:13 +0600

    enum completed

---
+++ user-types/enumType.ts
@@ -0,0 +1,18 @@
+enum cardinalDirections {
+    North=1,
+    East=4,
+    South=8,
+    West
+};
+
+console.log(cardinalDirections);
+
+enum seasons {
+    Spring="spring",
+    Summer="Summer",
+    Fall="Fall",
+    id =2,
+}
+
+console.log(seasons);
+
