9e31c73a7e4023c1539ed9137d38da759e084f86
ishanrana363 <ishanrana363@gmail.com>
2025-02-07 08:33:11 +0600

    build in data type

---
+++ built-in-type/index.js
@@ -0,0 +1,13 @@
+// number, string, boolean, void, null, undefined, never
+var firstName;
+var age;
+var adult;
+firstName = "Ishan";
+age = 21;
+adult = true;
+console.log(firstName, age, adult);
+function myFun() {
+    console.log("Hello type script");
+}
+;
+myFun();
---
+++ built-in-type/index.ts
@@ -0,0 +1,17 @@
+// number, string, boolean, void, null, undefined, never
+
+let firstName : string;
+let age : number;
+let adult : boolean;
+firstName = "Ishan";
+age = 21;
+adult = true
+
+console.log(firstName,age,adult);
+
+
+function myFun ():void{
+    console.log("Hello type script")
+};
+
+myFun()
