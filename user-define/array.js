ed9ef87a8ce60b6e3002a0757e71592d2f8cb577
ishanrana363 <ishanrana363@gmail.com>
2025-02-07 09:16:34 +0600

    array

--- user-define/array.js
+++ user-define/array.js
@@ -1,16 +1,8 @@
-
 // let userNames : string[];
 // userNames = ["a","b","c","d"];
-
 // let userNames : Array<string>;
-
 // userNames = ["a","b","c","d"];
 // console.log(userNames);
-
-
-let userNames: (string | number)[];
-userNames = ["a", "b", "c", "d",1,2,3,4];
-console.log(userNames)
-
-
-
+var userNames;
+userNames = ["a", "b", "c", "d", 1, 2, 3, 4];
+console.log(userNames);
