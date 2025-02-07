1ab96aa157894f84d9779a79671d3b45a6a16a37
ishanrana363 <ishanrana363@gmail.com>
2025-02-07 08:51:16 +0600

    aded

---
+++ first-programme/index.js
@@ -0,0 +1,5 @@
+function addd(num1, num2) {
+    console.log(num1 + num2);
+}
+addd(20, 20);
+addd(20, 20);
---
+++ first-programme/index.ts
@@ -0,0 +1,7 @@
+function addd (num1:number,num2:number){
+    console.log(num1+num2)
+}
+
+
+addd(20,20)
+addd(20,20)
